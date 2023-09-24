5d93a9184433f41f99b197da4df446f9a80fa70a
rrsharma7 <rahul.ranyal@hotmail.com>
2023-09-24 23:21:35 +0530

    Part 3: Solution

--- sharma_rahul__C3_Project/src/test/java/RestaurantTest.java
+++ sharma_rahul__C3_Project/src/test/java/RestaurantTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,8 +68,10 @@ class RestaurantTest {
     @Test
     public void select_item_from_list_should_return_order_cost(){
         int totalCost;
-        List<Item> selectedItems = null;
-        totalCost = restaurant.getTotalCost(selectedItems) ;
+        createMockRestaurant();
+        List<String> selectedItemNames = Arrays.asList("Sweet corn soup", "Vegetable lasagne" ) ;
+        totalCost = restaurant.getTotalCost(selectedItemNames) ;
+        assertEquals(388, totalCost);
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<GetCost>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
